14a79296fc3e3533365c4d0651c4df0a1286122f
hjc <jinchang.huang@sdlg.com.cn>
2016-08-24 16:34:38 +0800

    new bran1 commit

---
+++ src/com/hjc/newBran1/LearnGitBrance1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.hjc.newBran1;
+
+/**
+ *
+ * @author Administrator
+ *2016-8-24 下午04:32:14
+ */
+public class LearnGitBrance1 {
+
+	/**
+	 * @author hjc
+	 * 2016-8-24下午04:32:14
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("JJA");
+	}
+
+}
